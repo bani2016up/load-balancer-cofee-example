9ba2a492b759b98090272bf82ccd975cdfeb458f
BorisBabaian <april252423@gmail.com>
2025-04-15 12:32:43 +0300

    seminar, without enum class

--- include/products/product.h
+++ include/products/product.h
@@ -2,14 +2,17 @@
 #include <string>
 
 
- enum class ProductProcessStatus {
+enum class ProductProcessStatus
+{
     PENDING,
     PROCESSING,
     FINISHED
- };
+};
 
 
-class Product {
+class Product
+{
+private:
     const uint64_t id;
     uint64_t price;
     uint64_t processTime;
@@ -18,15 +21,19 @@ class Product {
 
 
 public:
-    Product(std::string name, uint64_t price = 0, uint64_t processTime = 0, uint64_t id = lastId);
+    Product(const std::string& name, uint64_t price = 0, uint64_t processTime = 0, uint64_t id = lastId);
 
     uint64_t getPrice() const;
     uint64_t getProcessTime() const;
     ProductProcessStatus getStatus() const;
     void processProduct();
+    virtual std::string getInfo() const;
+    void printInfo() const;
+
+    static std::string stat2s(ProductProcessStatus stat);
+
 
     static uint64_t lastId;
-
 };
 
 
@@ -34,6 +41,32 @@ public:
 
 
 
+class Beverage : public Product
+{
 
-// class Beverage : public Product {};
-// class Food : public Product {};
+    int volume;
+
+
+public:
+    Beverage(const std::string& name, uint64_t price = 90, uint64_t processTime = 60, uint64_t id = lastId, uint64_t volume = 250);
+
+    std::string getInfo() const;
+
+};
+
+
+
+class Food : public Product
+{
+
+    std::string type;
+
+public:
+    Food(const std::string& name, uint64_t price = 120, uint64_t processTime = 20, uint64_t id = lastId, const std::string& type = "BLUDO");
+
+    std::string getInfo() const;
+
+};
+
+
+
--- src/products/product.cpp
+++ src/products/product.cpp
@@ -1,11 +1,40 @@
 #include "products/product.h"
+#include <cctype>
+#include <format>
+#include <iostream>
 
 uint64_t Product::lastId = 0;
 
-Product::Product(std::string name, uint64_t price, uint64_t processTime, uint64_t id): id(id), price(price), processTime(processTime), name(name), status(ProductProcessStatus::PENDING)
+Product::Product(const std::string& name, uint64_t price, uint64_t processTime, uint64_t id) : id(id), price(price), processTime(processTime), name(name), status(ProductProcessStatus::PENDING)
 {
     ++lastId;
 }
+
+std::string Product::stat2s(ProductProcessStatus stat)
+{
+    switch (stat)
+    {
+        case ProductProcessStatus::PENDING:
+            return "PENDING";
+        case ProductProcessStatus::PROCESSING:
+            return "PROCESSING";
+        case ProductProcessStatus::FINISHED:
+            return "FINISHED";
+        default:
+            return "NO STATUS";
+    }
+}
+
+std::string Product::getInfo() const
+{
+    return std::format("status: {}\nname: {}\nprice: {}\ttime to cook: {}", stat2s(status), name, price, processTime);
+}
+
+void Product::printInfo() const
+{
+    std::cout << getInfo() << '\n' << '\n';
+}
+
 
 uint64_t Product::getPrice() const
 {
@@ -20,4 +49,26 @@ uint64_t Product::getProcessTime() const
 ProductProcessStatus Product::getStatus() const
 {
     return status;
+}
+
+
+Beverage::Beverage(const std::string& name, uint64_t price, uint64_t processTime, uint64_t id, uint64_t volume) : Product(name, price, processTime, id), volume(volume) {}
+
+std::string Beverage::getInfo() const
+{
+    return Product::getInfo() + std::format("\tvolume: {}", volume);
+
+}
+
+Food::Food(const std::string& name, uint64_t price, uint64_t processTime, uint64_t id, const std::string& type) : Product(name, price, processTime, id), type(type)
+{
+    for (char& c: this->type)
+    {
+        c = std::tolower(c);
+    }
+}
+
+std::string Food::getInfo() const
+{
+    return Product::getInfo() + std::format("\ttype: {}", type);
 }
